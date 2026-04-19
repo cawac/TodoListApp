b68d2b6b3614fb8584b2c68a03d483298cade9d7
cawac <cawapl33@gmail.com>
2026-04-19 19:13:16 +0300

    Add project files.

---
+++ WebApi/Data/TodoListDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Data
+{
+    public class TodoListDbContext : DbContext
+    {
+        public TodoListDbContext(DbContextOptions<TodoListDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoList> TodoLists { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoList>().HasData(
+                new TodoList { Id = 1, Title = "Work" },
+                new TodoList { Id = 2, Title = "Home" },
+                new TodoList { Id = 3, Title = "Study" }
+            );
+        }
+    }
+}
---
+++ WebApi/Migrations/20260419155836_InitialCreate.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoLists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoLists", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TodoLists",
+                columns: new[] { "Id", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Work" },
+                    { 2, "Home" },
+                    { 3, "Study" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoLists");
+        }
+    }
+}
